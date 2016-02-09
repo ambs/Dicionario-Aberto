c092b51439b6c67227b4819396d0f12da0a81af1
ambs <ambs@cpan.org>
2016-02-09 21:45:55 +0000

    working, but needing a rewrite

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,6 +2,6 @@
 angular.module('DA', [])
 	.controller('NewsController', function($scope, $http) {
 		$http.get("http://api.dicionario-aberto.net/news").then(function(response) {
-			$scope.news = response;
+			$scope.news = response.data;
 		});
 	});
