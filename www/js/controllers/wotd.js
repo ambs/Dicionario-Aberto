0e54aac76faffb478e1c9fae8f9aa3f2ea58a09b
ambs <ambs@cpan.org>
2016-02-21 17:22:59 +0000

    modularize formatter

--- www/js/controllers/wotd.js
+++ www/js/controllers/wotd.js
@@ -6,25 +6,7 @@ $mod.controller('DailyWord', function($scope, $http, $sce) {
 		$http.get("http://api.dicionario-aberto.net/wotd").then(function(response) {
 			var xml = response.data['xml'];
 			
-			var div   = function(c) { return el("div", c); }
-			var empty = function()  { return "" }
-			var id    = function(c) { return c }
-			$scope.def = $sce.trustAsHtml(dt(xml, {
-				"orth"      : empty,
-				
-				"#document" : id,
-				"#text"     : function(c) { return c.replace(/^[\s\n]+|[\s\n]+$/g, "")},
-
-				"entry"     : div,
-				"form"      : div,
-				"sense"     : div,
-				"etym"      : div,
-
-				"gramGrp"   : function(c) { return el("div", el("i", c)) },
-				
-				"#default"  : function(c,q,v) { return el("b",q) + ": " + c },
-				"def"     : function(c) { return div(c.replace(/_([^ ][^_]+)_/g, el("i", "$1")).replace(/\n\s*\n/g,"\n").replace(/\n/g,"<br/>")); }
-			}));
+			$scope.def = $sce.trustAsHtml(format_entry(xml));
 			
 			var title = xml.replace(/<\/orth>(.|\n)*$/,"");
 			title = title.replace(/^(.|\n)*<orth>/,"");
---
+++ www/js/formatter.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function format_entry(xml) {
+
+	var div   = function(c) { return el("div", c); }
+	var empty = function()  { return "" }
+	var id    = function(c) { return c }
+	
+	return dt(xml, {
+		"orth"      : empty,
+		
+		"#document" : id,
+		"#text"     : function(c) { return c.replace(/^[\s\n]+|[\s\n]+$/g, "")},
+
+		"entry"     : div,
+		"form"      : div,
+		"sense"     : div,
+		"etym"      : div,
+
+		"gramGrp"   : function(c) { return el("div", el("i", c)) },
+		
+		"#default"  : function(c,q,v) { return el("b",q) + ": " + c },
+		"def"       : function(c)     { 
+			return div(c.replace(/_([^ ][^_]+)_/g, el("i", "$1"))
+				        .replace(/\n\s*\n/g,"\n").replace(/\n/g,"<br/>")); }
+});}
