1969011193b3b6c9c6c5c96544067aaaa06c2246
ambs <ambs@cpan.org>
2016-02-21 18:09:20 +0000

    Better formatter

--- www/js/formatter.js
+++ www/js/formatter.js
@@ -1,5 +1,9 @@
 'use strict';
 
+function fix_italics (c) {
+	return c.replace(/_([^ ][^_]+)_/g, el("i", "$1"));
+}
+
 function format_entry(xml) {
 
 	var div   = function(c) { return el("div", c); }
@@ -15,13 +19,13 @@ function format_entry(xml) {
 		"entry"     : div,
 		"form"      : div,
 		"sense"     : div,
-		"etym"      : div,
+		"etym"      : function(c) { return div(fix_italics(c))},
 
 		"gramGrp"   : function(c) { return el("div", el("i", c)) },
 		
 		"#default"  : function(c,q,v) { return el("b",q) + ": " + c },
 		"def"       : function(c)     { 
-			return div(c.replace(/_([^ ][^_]+)_/g, el("i", "$1"))
+			return div(fix_italics(c)
 				        .replace(/\n\s*\n/g,"\n").replace(/\n/g,"<br/>")); }
 });
 }
