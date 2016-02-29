468edf5209ea42e283e7bfafae2fd4d8915292ee
Nuno Carvalho <mestre.smash@gmail.com>
2016-02-29 12:10:18 +0000

    Add loader to random page

--- www/js/controllers/random.js
+++ www/js/controllers/random.js
@@ -3,11 +3,15 @@
 var $mod = angular.module('DA');
 
 $mod.controller('Random', function($scope, $http, $sce, API) {
-		$http.get(API + "/random").then(function(response) {
-			var xml = response.data['xml'];
+  $scope.loading = true;
+
+  $http.get(API + "/random").then(function(response) {
+    var xml = response.data['xml'];
 			
-			$scope.def = $sce.trustAsHtml(format_entry(xml));
-			$scope.word = $sce.trustAsHtml(get_title(xml));
-		});
-	});
+    $scope.def = $sce.trustAsHtml(format_entry(xml));
+    $scope.word = $sce.trustAsHtml(get_title(xml));
+    $scope.loading = false;
+  });
+
+});
 
