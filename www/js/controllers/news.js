6123b03b26573f25ff0d9a069d4e5d96a95f8f17
ambs <ambs@cpan.org>
2016-03-02 21:26:50 +0000

    Loading icons for word of the day and news.

--- www/js/controllers/news.js
+++ www/js/controllers/news.js
@@ -2,13 +2,18 @@
 
 var $mod = angular.module('DA');
 
-$mod.controller('NewsController', function($scope, $http, $sce, API) {
-		$http.get(API + "/news?limit=4").then(function(response) {
-			$scope.news = 
-				$.map(response.data,
-					function (x) { x['text'] = $sce.trustAsHtml(x['text']); return x});
-		});
-	});
+$mod.controller('NewsController',
+	function($scope, $http, $sce, API) {
+	  $scope.loading = true;
+	  $http.get(API + "/news?limit=4").then(
+	  	function(response) {
+		  $scope.news = $.map(response.data, function (x) {
+		    x['text'] = $sce.trustAsHtml(x['text']);
+		    return x;
+		  });
+	  	  $scope.loading = false;
+	  });
+});
 
 $mod.filter('myDate', function() {
 	return function (date) {
--- www/js/controllers/wotd.js
+++ www/js/controllers/wotd.js
@@ -3,11 +3,14 @@
 var $mod = angular.module('DA');
 
 $mod.controller('DailyWord', function($scope, $http, $sce, API) {
-		$http.get(API + "/wotd").then(function(response) {
+  $scope.loading = true;
+
+  $http.get(API + "/wotd").then(function(response) {
 			var xml = response.data['xml'];
 			
 			$scope.def = $sce.trustAsHtml(format_entry(xml));
 			$scope.word = $sce.trustAsHtml(get_title(xml));
+            $scope.loading = false;
 		});
 	});
 
