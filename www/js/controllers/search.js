8126395a8e1bed46f7e87914c4b9e0f84727653a
ambs <ambs@cpan.org>
2016-03-03 14:24:49 +0000

    Loading...

--- www/js/controllers/search.js
+++ www/js/controllers/search.js
@@ -3,7 +3,8 @@
 var $mod = angular.module('DA');
 
 $mod.controller('Search', function($scope, $http, $sce, $stateParams, API) {
-
+	$scope.mainLoading = true;
+	$scope.nearLoading = true;
 	var word = $stateParams.word;
 
 	$scope.entries = [];
@@ -17,12 +18,14 @@ $mod.controller('Search', function($scope, $http, $sce, $stateParams, API) {
 				  		     "word" : $sce.trustAsHtml(get_title(x.xml)) };
 			});
 		}
+		$scope.mainLoading = false;
 	});
 
 	$http.get(API + "/near/" + word).then(function(response) {
 		if (response.data.length > 0) {
 			$scope.near = response.data;
 		}
+		$scope.nearLoading = false;		
 	});
 
 });
