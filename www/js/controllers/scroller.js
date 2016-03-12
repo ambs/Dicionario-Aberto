e35924b81af8cb33731d68bd5a005fef09040978
ambs <ambs@cpan.org>
2016-03-12 14:53:25 +0000

    intro do dicionario cm links relativos

---
+++ www/js/controllers/scroller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+app.controller('Scroller', function($scope, $location, $anchorScroll) {
+   $scope.scrollTo = function(id) {
+      $location.hash(id);
+      $anchorScroll();
+   }
+});
