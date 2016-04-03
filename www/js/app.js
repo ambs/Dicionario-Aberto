08526ea8548455fbc3059224b0714eee6acf936e
ambs <ambs@cpan.org>
2016-04-03 15:57:00 +0100

    Further bootstrap work

--- www/js/app.js
+++ www/js/app.js
@@ -2,6 +2,6 @@
 'use strict';
 
 var app = angular
-  .module('DA', ['ui.router','highcharts-ng'])
+  .module('DA', ['ui.router','highcharts-ng','ui.bootstrap'])
   .constant('API', 'http://api.dicionario-aberto.net');
 
