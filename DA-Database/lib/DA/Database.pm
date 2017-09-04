3854c017e5d10246254feba8a8b02631a46f6462
Alberto Simões <ambs@cpan.org>
2017-09-04 17:07:48 +0100

    proper indentations. Emacs is back

--- DA-Database/lib/DA/Database.pm
+++ DA-Database/lib/DA/Database.pm
@@ -32,56 +32,53 @@ our @LETTERS = qw!a b c d e f g h i j k l m n o p q r s t u v w x y z
                   ã õ á é í ó ú à è ì ò ù â ê î ô û ç ä ë ï ö ü ÿ ý ũ ẽ -!;
 
 sub new {
-	my $class = shift;
+  my $class = shift;
 
-	my $self = bless {}, $class;
+  my $self = bless {}, $class;
 
-	my $thing = shift;
+  my $thing = shift;
 
 
-	if (ref($thing) eq "CODE") {
-		$self->{dbh_sub} = $thing;
-	}
-	elsif (blessed($thing) && $thing->can("prepare")) {
-		$self->{dbh} = $thing;
-	} 
-	else {
-		$self->{dbh} = DBI->connect($thing, @_);
-	}
+  if (ref($thing) eq "CODE") {
+    $self->{dbh_sub} = $thing;
+  }
+  elsif (blessed($thing) && $thing->can("prepare")) {
+    $self->{dbh} = $thing;
+  }
+  else {
+    $self->{dbh} = DBI->connect($thing, @_);
+  }
 
-	return bless $self, $class;
+  return bless $self, $class;
 }
 
 sub dbh {
-	my ($self) = shift;
+  my ($self) = shift;
 
-	if (exists($self->{dbh_sub})) {
-		return $self->{dbh_sub}->();
-	}
-	if (exists($self->{dbh})) {
-		return $self->{dbh};
-	}
-	die "no DBH?";
+  if (exists($self->{dbh_sub})) {
+    return $self->{dbh_sub}->();
+  }
+  if (exists($self->{dbh})) {
+    return $self->{dbh};
+  }
+  die "no DBH?";
 }
 
 sub affixes {
-    my ($self, $type, $query, $n) = @_;
-    $type eq "infix"  and $query = "\%_${query}_%";
-    $type eq "suffix" and $query = "\%_${query}";
-    $type eq "prefix" and $query = "${query}_%";
+  my ($self, $type, $query, $n) = @_;
+  $type eq "infix"  and $query = "\%_${query}_%";
+  $type eq "suffix" and $query = "\%_${query}";
+  $type eq "prefix" and $query = "${query}_%";
 
-    $n = $n ? "LIMIT $n" : "";
+  $n = $n ? "LIMIT $n" : "";
 
-    my $sth = $self->dbh->prepare(<<"---");
+  my $sth = $self->dbh->prepare(<<"---");
           SELECT word, sense, preview FROM word INNER JOIN preview_cache ON word.word_id = preview_cache.word_id WHERE deleted = 0 AND word LIKE ? ORDER BY normalized $n
 ---
-    $sth->execute($query);
-    return $sth->fetchall_arrayref({});
+  $sth->execute($query);
+  return $sth->fetchall_arrayref({});
 }
 
-sub prefix {
-  my ($self, $word) = @_;
-}
 
 sub random {
   my ($self) = @_;
@@ -95,38 +92,38 @@ sub random {
 }
 
 sub revision_from_wid {
-	my ($self, $wid) = @_;
+  my ($self, $wid) = @_;
 
-	my $sth = $self->dbh->prepare(<<"---");
+  my $sth = $self->dbh->prepare(<<"---");
  SELECT `xml` FROM `word` INNER JOIN `revision` 
      ON `word`.`word_id` = `revision`.`word_id`
       AND `word`.`last_revision` = `revision`.`revision_id` 
      WHERE `word`.`word_id` = ?;
 ---
 
-	$sth->execute($wid);
-	my ($xml) = $sth->fetchrow_array;
-	return $xml;
+  $sth->execute($wid);
+  my ($xml) = $sth->fetchrow_array;
+  return $xml;
 }
 
 sub authenticate {
-    my ($self, $user, $password) = @_;
-    $password = md5_hex $password;
+  my ($self, $user, $password) = @_;
+  $password = md5_hex $password;
 
-    my $sth = $self->dbh->prepare(<<"---");
+  my $sth = $self->dbh->prepare(<<"---");
     SELECT username, email FROM user WHERE username = ? AND password = ? and banned = 0;
 ---
-    $sth->execute($user, $password);
-    my @row = $sth->fetchrow_array();
-    return @row ? { usertype => 1, username => $user, avatar => md5_hex(_n($row[1]))} : undef;
+  $sth->execute($user, $password);
+  my @row = $sth->fetchrow_array();
+  return @row ? { usertype => 1, username => $user, avatar => md5_hex(_n($row[1]))} : undef;
 }
 
 sub _n {
-    my $w = shift;
-    $w =~ s/^\s*//;
-    $w =~ s/\s*$//;
-    $w = lc $w;
-    return $w;
+  my $w = shift;
+  $w =~ s/^\s*//;
+  $w =~ s/\s*$//;
+  $w = lc $w;
+  return $w;
 }
 
 
@@ -175,7 +172,6 @@ sub get_browse_letter_position {
   $sth->execute($l);
   my @ans = $sth->fetchrow_array;
   return $ans[0] if @ans;
-  
 
   $sth = $self->dbh->prepare('CALL getClosestMatch(?);');
   $sth->execute($l);
@@ -188,7 +184,7 @@ sub wotd {
     my ($self) = @_;
     my $sth = $self->dbh->prepare("SELECT `value` FROM `metadata` WHERE `key` = ?");
     $sth->execute('wotd');
-    
+
     my ($wid) = $sth->fetchrow_array;
 
     return $self->revision_from_wid($wid);
@@ -197,9 +193,9 @@ sub wotd {
 sub metadata {
   my ($self, $key) = @_;
   return undef unless grep {$key eq $_} qw.count first_word last_word wotd.;
-  my $query = <<"";
+  my $query = <<"---";
     SELECT `value` FROM `metadata` WHERE `key` = ?
-
+---
   my $sth = $self->dbh->prepare($query);
   $sth->execute($key);
   my ($ans) = $sth->fetchrow_array;
@@ -207,45 +203,44 @@ sub metadata {
 }
 
 sub retrieve_entry {
-	my ($self, $word, $n) = @_;
+  my ($self, $word, $n) = @_;
 
-	my $query = <<"";
+  my $query = <<"---";
 SELECT * FROM word INNER JOIN revision 
   ON word.last_revision = revision.revision_id AND word.word_id = revision.word_id
   WHERE word=? %%AND%% AND word.deleted=0;
+---
+  $query =~ s/%%AND%%/ $n ? "AND sense=?" : "" /e;
 
-	$query =~ s/%%AND%%/ $n ? "AND sense=?" : "" /e;
+  my $sth = $self->dbh->prepare($query);
+  $sth->execute($word, $n ? $n : ());
 
-	my $sth = $self->dbh->prepare($query);
-	$sth->execute($word, $n ? $n : ());
-
-	return $sth->fetchall_arrayref({});
-
+  return $sth->fetchall_arrayref({});
 }
 
 sub retrieve_news {
-	my ($self, %filter) = @_;
+  my ($self, %filter) = @_;
 
-	my @where = ();
-	if (exists($filter{id}) && $filter{id} =~ /^\d+$/) {
-		push @where, "WHERE idnew = $filter{id}"
-	}
+  my @where = ();
+  if (exists($filter{id}) && $filter{id} =~ /^\d+$/) {
+    push @where, "WHERE idnew = $filter{id}"
+  }
 
-	push @where, "ORDER BY date DESC";
+  push @where, "ORDER BY date DESC";
 
-	if (exists($filter{limit}) && $filter{limit} =~ /^\d+$/) {
-		push @where, "LIMIT $filter{limit}"
-	}
+  if (exists($filter{limit}) && $filter{limit} =~ /^\d+$/) {
+    push @where, "LIMIT $filter{limit}"
+  }
 
-	my $sql = "SELECT idnew, user, date, title, text FROM new";
-	$sql = join(" ", $sql, @where);
+  my $sql = "SELECT idnew, user, date, title, text FROM new";
+  $sql = join(" ", $sql, @where);
 
-	my $sth = $self->dbh->prepare($sql);
-	$sth->execute();
+  my $sth = $self->dbh->prepare($sql);
+  $sth->execute();
 
-	my $news = $sth->fetchall_arrayref({});
+  my $news = $sth->fetchall_arrayref({});
 
-	return $news;
+  return $news;
 }
 
 
@@ -253,82 +248,82 @@ sub retrieve_news {
 
 
 sub _delete_queries {
-    my @ans;
-    for (0..length($_[0])-1) {
-        my $x = $_[0];
-        substr($x,$_,1) = "";
-        push @ans, $x;
-    }
-    \@ans;
+  my @ans;
+  for (0..length($_[0])-1) {
+    my $x = $_[0];
+    substr($x,$_,1) = "";
+    push @ans, $x;
+  }
+  \@ans;
 }
 
 sub _trs_queries {
-    my @ans;
-    for (0..length($_[0])-2) {
-        my $x = $_[0];
-        (substr($x,$_,1), substr($x,$_+1,1)) = (substr($x,$_+1,1),substr($x,$_,1));
-        push @ans, $x;
-    }
-    \@ans;
+  my @ans;
+  for (0..length($_[0])-2) {
+    my $x = $_[0];
+    (substr($x,$_,1), substr($x,$_+1,1)) = (substr($x,$_+1,1),substr($x,$_,1));
+    push @ans, $x;
+  }
+  \@ans;
 }
 
 sub _replace_queries {
-    my @ans;
-    for (0..length($_[0])-1) {
-        my $x = $_[0];
-        substr($x,$_,1) = "_";
-        push @ans, $x;
-    }
-    \@ans;
+  my @ans;
+  for (0..length($_[0])-1) {
+    my $x = $_[0];
+    substr($x,$_,1) = "_";
+    push @ans, $x;
+  }
+  \@ans;
 }
 
 sub _add_queries {
-    my @ans;
-    push @ans, "_$_[0]";
-    for (1..length($_[0])-1) {
-        my $x = $_[0];
-        substr($x,$_,0) = "_";
-        push @ans, $x;
-    }
-    push @ans, "$_[0]_";
-    \@ans;
+  my @ans;
+  push @ans, "_$_[0]";
+  for (1..length($_[0])-1) {
+    my $x = $_[0];
+    substr($x,$_,0) = "_";
+    push @ans, $x;
+  }
+  push @ans, "$_[0]_";
+  \@ans;
 }
 
 sub near_misses {
-    my ($self, $word) = @_;
+  my ($self, $word) = @_;
 
-    my %WORDS;
+  my %WORDS;
 
-    my $deletions = _delete_queries($word);
-    my $trs       = _trs_queries($word);
-    $WORDS{$_}++ for (@$deletions, @$trs);
+  my $deletions = _delete_queries($word);
+  my $trs       = _trs_queries($word);
+  $WORDS{$_}++ for (@$deletions, @$trs);
 
-    my $replaces  = _replace_queries($word);
-    my $additions = _add_queries($word);
-    for my $word (@$replaces, @$additions) {
-        for my $letter (@LETTERS) {
-            my $x = $word;
-            $x =~ s/_/$letter/;
-            $WORDS{$x}++
-        }
+  my $replaces  = _replace_queries($word);
+  my $additions = _add_queries($word);
+  for my $word (@$replaces, @$additions) {
+    for my $letter (@LETTERS) {
+      my $x = $word;
+      $x =~ s/_/$letter/;
+      $WORDS{$x}++
     }
+  }
 
-    my $query = join(", ", map { "'$_'" } keys %WORDS);
-    %WORDS = ();
+  my $query = join(", ", map { "'$_'" } keys %WORDS);
+  %WORDS = ();
 
-    $query = "SELECT DISTINCT(word) FROM word WHERE word IN ($query)";
-    my $sth = $self->dbh->prepare($query);
-    return [] unless $sth;
+  $query = "SELECT DISTINCT(word) FROM word WHERE word IN ($query)";
+  my $sth = $self->dbh->prepare($query);
+  return [] unless $sth;
 
-    $sth->execute();
+  $sth->execute();
 
-    my $val;
-    my @ANS = ();
-    while( ($val) = $sth->fetchrow_array) {
-        push @ANS, $val;
-    }
+  my $val;
+  my @ANS = ();
+  while( ($val) = $sth->fetchrow_array) {
+    push @ANS, $val;
+  }
 
-    return \@ANS;
+  return \@ANS;
 }
 
 sub words_by_letter {
@@ -384,105 +379,108 @@ sub moderation_stats {
                                                  group by letter order by letter;
 ---
 
-    for ('a'..'z') {
-        my $t = $totals->{$_}{'COUNT(word)'};
-        my $d = $deleted->{$_}{'COUNT(word)'} || 0;
-        my $m = $moderated->{$_}{'COUNT(word)'} || 0;
+  for ('a'..'z') {
+    my $t = $totals->{$_}{'COUNT(word)'};
+    my $d = $deleted->{$_}{'COUNT(word)'} || 0;
+    my $m = $moderated->{$_}{'COUNT(word)'} || 0;
 
-        push @$D => 0+$d;
-        push @$M => 0+$m;
-        push @$T => 0+($t-$d-$m);
-    }
+    push @$D => 0+$d;
+    push @$M => 0+$m;
+    push @$T => 0+($t-$d-$m);
+  }
 
-    return { letters => ['a'..'z'],
-             data => [
-                      { name => 'apagadas',    color => '#BB6666', data => $D },
-                      { name => 'aprovadas',   color => '#45A772', data => $M },
-                      { name => 'por moderar', color => '#4572A7', data => $T },
-                     ]
-           }; 
+  return { letters => ['a'..'z'],
+	   data => [
+		    { name => 'apagadas',    color => '#BB6666', data => $D },
+		    { name => 'aprovadas',   color => '#45A772', data => $M },
+		    { name => 'por moderar', color => '#4572A7', data => $T },
+		   ]
+	 };
 }
 
 sub recover_password {
-    my ($self, $data) = @_;
-    my $q = "SELECT username, email FROM user WHERE " . ($data =~ /@/ ? "email" : "username") . " = ?";
-    my $sth = $self->dbh->prepare($q);
-    $sth->execute($data);
-    my $records = $sth->fetchall_arrayref;
-    if (@$records) {
-	for my $u (@$records) {
-	    my ($username, $email) = ($u->[0], $u->[1]);
-	    my $md5 = md5_hex("$username ".localtime);
-	    $self->quick_delete('user_restore', { user => $username });
-	    $self->quick_insert('user_restore', {
-		md5   => $md5, user  => $username,
-		new   => 0, email => $email, name  => "" });
+  my ($self, $data) = @_;
+  my $q = "SELECT username, email FROM user WHERE " . ($data =~ /@/ ? "email" : "username") . " = ?";
+  my $sth = $self->dbh->prepare($q);
+  $sth->execute($data);
+  my $records = $sth->fetchall_arrayref;
+  if (@$records) {
+    for my $u (@$records) {
+      my ($username, $email) = ($u->[0], $u->[1]);
+      my $md5 = md5_hex("$username ".localtime);
+      $self->quick_delete('user_restore', { user => $username });
+      $self->quick_insert('user_restore', {
+					   md5   => $md5, user  => $username,
+					   new   => 0, email => $email, name  => "" });
 
-	    return { email => $email, username => $username, md5 => $md5 };
-	}
-    } else {
-	return undef;
+      return { email => $email, username => $username, md5 => $md5 };
     }
+  } else {
+    return undef;
+  }
 }
 
 sub register_user {
-    my ($self, $data) = @_;
-    my ($username, $email, $name) = ($data->{username}, $data->{email}, $data->{name});
+  my ($self, $data) = @_;
+  my ($username, $email, $name) = ($data->{username}, $data->{email}, $data->{name});
 
-    return undef if $self->user_exists($username);
-    return undef if $self->email_exists($email);
-    
-    my $md5 = md5_hex("$username ". localtime);
-    $self->quick_delete( user_restore => { user => $username });
-    $self->quick_insert( user_restore => { 
-	md5 => $md5, user => $username, new => 1, email => $email, name => $name });
+  return undef if $self->user_exists($username);
+  return undef if $self->email_exists($email);
 
-    return { email => $email, username => $username, md5 => $md5 };
+  my $md5 = md5_hex("$username ". localtime);
+  $self->quick_delete( user_restore => { user => $username });
+  $self->quick_insert( user_restore =>
+		       {
+			md5 => $md5,
+			user => $username,
+			new => 1,
+			email => $email,
+			name => $name });
+  return { email => $email, username => $username, md5 => $md5 };
 }
 
 sub user_exists {
-    my ($self, $username) = @_;
-    my $sth = $self->dbh->prepare("SELECT * FROM user WHERE username = ?");
-    $sth->execute($username);
-    my $records = $sth->fetchall_arrayref;
-    return @$records ? 1 : 0;
+  my ($self, $username) = @_;
+  my $sth = $self->dbh->prepare("SELECT * FROM user WHERE username = ?");
+  $sth->execute($username);
+  my $records = $sth->fetchall_arrayref;
+  return @$records ? 1 : 0;
 }
 
 
 sub email_exists {
-    my ($self, $email) = @_;
-    my $sth = $self->dbh->prepare("SELECT * FROM user WHERE email = ?");
-    $sth->execute($email);
-    my $records = $sth->fetchall_arrayref;
-    return @$records ? 1 : 0;
+  my ($self, $email) = @_;
+  my $sth = $self->dbh->prepare("SELECT * FROM user WHERE email = ?");
+  $sth->execute($email);
+  my $records = $sth->fetchall_arrayref;
+  return @$records ? 1 : 0;
 }
 
 
 sub quick_insert {
-    my ($self, $table, $data) = @_;
-    my (@data, @where);
-    foreach my $c (keys %$data) {
-	push @data, $data->{$c};
-	push @where, $c;
-    }
-    my $columns = join(",", @where);
-    my $qmarks  = join(",", ("?") x @where);
-    my $sth = $self->dbh->prepare("INSERT INTO $table ($columns) VALUES ($qmarks);");
-    $sth->execute(@data);
+  my ($self, $table, $data) = @_;
+  my (@data, @where);
+  foreach my $c (keys %$data) {
+    push @data, $data->{$c};
+    push @where, $c;
+  }
+  my $columns = join(",", @where);
+  my $qmarks  = join(",", ("?") x @where);
+  my $sth = $self->dbh->prepare("INSERT INTO $table ($columns) VALUES ($qmarks);");
+  $sth->execute(@data);
 }
 
 sub quick_delete {
-    my ($self, $table, $constraints) = @_;
+  my ($self, $table, $constraints) = @_;
 
-    my (@data, @where);
-    foreach my $c (keys %$constraints) {
-	push @data, $constraints->{$c};
-	push @where, $c;
-    }
-    my $where = join (" AND ", map { "$_ = ?" } @where);
-    my $sth = $self->dbh->prepare("DELETE FROM $table WHERE $where;");
-    $sth->execute(@data);
-    
+  my (@data, @where);
+  foreach my $c (keys %$constraints) {
+    push @data, $constraints->{$c};
+    push @where, $c;
+  }
+  my $where = join (" AND ", map { "$_ = ?" } @where);
+  my $sth = $self->dbh->prepare("DELETE FROM $table WHERE $where;");
+  $sth->execute(@data);
 }
 
 =head1 AUTHOR
