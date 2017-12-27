930458057ca4d54924ff94b334ba037d928976e0
Alberto Simões <hashashin@gmail.com>
2017-12-27 20:51:11 +0000

    Remove old angular template

--- www-bootstrap/js/app.js
+++
@@ -1,7 +0,0 @@
-
-'use strict';
-
-var app = angular
-  .module('DA', ['ui.router','highcharts-ng','ui.bootstrap'])
-  .constant('API', 'http://api.dicionario-aberto.net');
-
--- www-bootstrap/js/controllers/scroller.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-app.controller('Scroller', function($scope, $location, $anchorScroll) {
-   $scope.scrollTo = function(id) {
-      $location.hash(id);
-      $anchorScroll();
-   }
-});
