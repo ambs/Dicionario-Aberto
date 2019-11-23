64b1b8bc25f782f1c41472d65861b878dc7eb859
Alberto Simoes <ambs@cpan.org>
2019-11-23 21:30:04 +0000

    Try to have adv search as publisc page

--- www/js/routes.js
+++ www/js/routes.js
@@ -61,6 +61,10 @@ my_routes.ss_search = function (data) {
 };
 
 
+my_routes.adv_search = function(data) {
+    load_template("adv_search", function(){});
+};
+
 my_routes.search = function(data) {
     load_template("search", function(){
 	$.ajax({
@@ -89,35 +93,6 @@ my_routes.search = function(data) {
     });
 };
 
-my_routes.xpassword = (params) => {
-	var hash = params.hash;
-	$.ajax({
-		url: 'https://api.dicionario-aberto.net/confirm/' + hash
-	}).done( (data) => {
-		if (data.status == "OK") {
-			var username = data.username;
-			load_template("password", ( tmpl ) => { $("#hash").value = hash; $("#user").value = username; });
-		}
-		else {
-			GO("/");
-			show_danger_alert(data.error);
-		}
-	}) 
-};
-
-my_routes.user = () => {
-    load_template('user', () => {});
-};
-
-my_routes.login = function() {
-    load_template("login", function() {
-	$('a[role="tab"]').click(function (e) {
-	    e.preventDefault();
-	    $(this).tab('show');
-	});
-    });
-};
-
 my_routes.root = () => {
     load_template("index", function() {
 	$.ajax({
@@ -137,14 +112,15 @@ my_routes.root = () => {
 
 function registerRoutes() {
     $.router.add('/', my_routes.root);
-    $.router.add('/login', my_routes.login);
+
     $.router.add('/search/:word', my_routes.search);
     $.router.add('/search/:word/:n', my_routes.search);
     $.router.add('/ss_search/:type/:word', my_routes.ss_search);
     $.router.add('/rev_search/:words', my_routes.rev_search);
     $.router.add('/ont_search/:words', my_routes.ont_search);
-    $.router.add('/user', my_routes.user);
-    $.router.add('/confirm/:hash', my_routes.xpassword);
+
+    $.router.add('/adv_search', my_routes.adv_search);
+    
     $.router.addErrorHandler( (url) => { GO('/'); });
     
     $( document ).ajaxStop(function() {
