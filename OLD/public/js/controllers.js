607e8a66c28b2937b6b644827376dc6972deede3
Alberto Simões <ambs@cpan.org>
2020-03-14 14:33:26 +0000

    remove old stuff

--- OLD/public/js/controllers.js
+++
@@ -1,7 +0,0 @@
-
-angular.module('DA', [])
-	.controller('NewsController', function($scope, $http) {
-		$http.get("/ajax/news").then(function(response) {
-			$scope.news = response.data;
-		});
-	});
